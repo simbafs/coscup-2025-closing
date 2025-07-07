66122af79198f20392c427e0df99daeb42828841
SimbaFs <simba.fs@gmail.com>
2025-07-07 16:03:56 +0800

    a script to fetch sponsor image

---
+++ script/sponsor.ts
@@ -0,0 +1,90 @@
+// some code copy from https://github.com/COSCUP/2025/blob/main/loaders/sponsor.data.ts
+
+import fs from 'fs/promises'
+import { parse } from 'csv-parse/sync'
+
+export interface Sponsor {
+	id: string
+	image: string
+	'name:zh-TW': string
+	'name:en': string
+	'intro:zh-TW': string
+	'intro:en': string
+	link: string
+	level: string
+	canPublish: 'Y' | 'N'
+}
+
+// 轉換 Google Drive 圖片 URL
+async function getDriveImageBase64(shareUrl: string): Promise<string> {
+	if (!shareUrl) {
+		return ''
+	}
+
+	const fileIdMatch = shareUrl.match(/(?:\/d\/|id=)([^/?]+)/)
+	const fileId = fileIdMatch ? fileIdMatch[1] : null
+	if (!fileId) return ''
+	const directUrl = `https://drive.google.com/uc?export=view&id=${fileId}`
+	// console.log(directUrl)
+	const response = await fetch(directUrl).catch(err => {
+		console.error(`failed to fetch ${directUrl}: ${err}`)
+	})
+	if (!response) {
+		return ''
+	}
+
+	if (!response.ok) {
+		throw new Error(`Failed to fetch image from Google Drive: ${response.statusText}`)
+	}
+
+	const arrayBuffer = await response.arrayBuffer()
+	const buffer = Buffer.from(arrayBuffer)
+	return `data:${response.headers.get('content-type') || 'image/jpeg'};base64,${buffer.toString('base64')}`
+}
+
+async function getSponsers(filename: string) {
+	if (!filename) {
+		throw new Error('Filename is required to fetch sponsors.')
+	}
+	try {
+		fs.stat(filename)
+	} catch (e) {
+		throw new Error(`file not found: ${e}`)
+	}
+
+	const content = await fs.readFile(filename, 'utf-8')
+	const records: Sponsor[] = parse(content, {
+		columns: true,
+	})
+
+	return records
+}
+
+async function downloadImages(sponsers: Sponsor[]): Promise<Record<string, string>> {
+	return Promise.all(sponsers.map(s => getDriveImageBase64(s.image)))
+		.then(images => images.map((image, index) => [sponsers[index].id, image]))
+		.then(Object.fromEntries)
+}
+
+async function main() {
+	const filename = process.argv[2]
+	if (!filename) {
+		console.error('Usage: node script/sponsor.js <filename>')
+		process.exit(1)
+	}
+
+	const sponsors = await getSponsers(filename)
+	const images = await downloadImages(sponsors)
+
+	const result = sponsors.map(s => ({
+		id: s.id,
+		name: s['name:zh-TW'],
+		image: images[s.id],
+	}))
+
+	const json = JSON.stringify(result)
+
+	console.log(json)
+}
+
+main().catch(console.error)
